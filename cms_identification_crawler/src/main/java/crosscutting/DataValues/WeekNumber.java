372de9f3f15d6665308d111f3bf54cea0b5e6ad4
Steven Treske <steven.treske@hotmail.com>
2017-08-22 02:21:47 +0200

    ValueObject für Kalenderwoche + Test hinzugefügt

---
+++ cms_identification_crawler/src/main/java/crosscutting/DataValues/WeekNumber.java
@@ -0,0 +1,21 @@
+package crosscutting.DataValues;
+
+import java.util.Calendar;
+
+public class WeekNumber {
+	private int weeknumber;
+	
+	public WeekNumber(Calendar calendar) {
+		this.setWeekNumber(calendar);
+	}
+
+	public int get() {
+		return weeknumber;
+	}
+
+	private void setWeekNumber(Calendar calender) {
+		this.weeknumber = calender.get(Calendar.WEEK_OF_YEAR);
+	}
+	
+	
+}
--- cms_identification_crawler/src/test/java/ValueObjectTests/WeekNumberTest.java
+++ cms_identification_crawler/src/test/java/ValueObjectTests/WeekNumberTest.java
@@ -1,14 +1,29 @@
 package ValueObjectTests;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.GregorianCalendar;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.junit.Test;
+
+import crosscutting.DataValues.Semester;
+
+
+import crosscutting.DataValues.WeekNumber;
 
 public class WeekNumberTest {
 	@Test
 	public void shouldReturnCorrectWeekNumber() {
+		// Arrange
+		Calendar calendar = new GregorianCalendar(2017, Calendar.AUGUST, 22);
 		
+		// Act
+		WeekNumber weeknumber = new WeekNumber(calendar);
+		
+		// Assert
+		assertEquals(34, weeknumber.get());
 	}
 }
