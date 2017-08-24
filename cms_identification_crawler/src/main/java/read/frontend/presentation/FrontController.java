092f15f3e5e8bf5641877e6af246450e320c0ca5
Steven Treske <steven.treske@hotmail.com>
2017-08-25 01:08:27 +0200

    [Hack] Über den FrontController die Moeglichkeit gegeben, die Crawler zu starten, wenn die Timer grünes Licht geben

--- cms_identification_crawler/src/main/java/read/frontend/presentation/FrontController.java
+++ cms_identification_crawler/src/main/java/read/frontend/presentation/FrontController.java
@@ -18,6 +18,14 @@ import read.frontend.presentation.Models.VeraenderungAktuelleWoche;
 import read.frontend.presentation.Queries.CMSHochschulen;
 import read.frontend.presentation.Queries.CMSVerbreitung;
 import read.frontend.presentation.Queries.VeraenderungWoche;
+import write.TimeEvents.CMSIdentifierCrawlerTimer;
+import write.TimeEvents.ICMSIdentifierCrawlerTimer;
+import write.TimeEvents.IUniversityCrawlerTimer;
+import write.TimeEvents.UniversityCrawlerTimer;
+import write.UniversityCrawler.IUniversityCrawler;
+import write.UniversityCrawler.UniversityCrawler;
+import write.cmsidentifier.business.CMSIdentifierCrawler;
+import write.cmsidentifier.business.ICMSIdentifierCrawler;
 
 @Path("/json")
 public class FrontController {
@@ -57,4 +65,33 @@ public class FrontController {
 		return (VeraenderungAktuelleWoche)resolver.Resolve(new VeraenderungWoche());
 	}
 	
+	@GET
+	@Path("/crawlUniversities")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response startUniCrawling(InputStream incomingData){
+		IUniversityCrawlerTimer universityCrawlerTimer = new UniversityCrawlerTimer();
+		
+		if (universityCrawlerTimer.start()) {
+			IUniversityCrawler universityCrawler = new UniversityCrawler();
+			universityCrawler.crawl();
+			return Response.status(200).entity("Universities done...").build();
+		}
+		
+		return Response.status(200).entity("Universities already crawled").build();
+	}
+	
+	@GET
+	@Path("/crawlCMS")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response startCMSCrawling(InputStream incomingData){
+		ICMSIdentifierCrawlerTimer cmsIdentifierTimer = new CMSIdentifierCrawlerTimer();
+		
+		if (cmsIdentifierTimer.start()) {
+			ICMSIdentifierCrawler cmdIdentifierCrawler = new CMSIdentifierCrawler();
+			cmdIdentifierCrawler.crawl();
+			return Response.status(200).entity("CmsIdentifierCrawler done...").build();
+		}
+		
+		return Response.status(200).entity("CMS already crawled").build();
+	}
 }
